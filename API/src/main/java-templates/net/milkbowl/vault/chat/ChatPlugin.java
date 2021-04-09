274b5fcef068112796125b4b9f5a21c9d986c177
egg82 <eggys82@gmail.com>
2021-04-08 20:03:08 -0600

    Add chat plugins

--- API/src/main/java-templates/net/milkbowl/vault/chat/ChatPlugin.java
+++ API/src/main/java-templates/net/milkbowl/vault/chat/ChatPlugin.java
@@ -1,28 +1,24 @@
-package net.milkbowl.vault.permission;
+package net.milkbowl.vault.chat;
 
 import org.jetbrains.annotations.NotNull;
 
 /**
- * Permission plugins available to Vault.
+ * Chat plugins available to Vault.
  */
-public enum PermissionPlugin {
+public enum ChatPlugin {
     B_PERMISSIONS("bPermissions", "${bpermissions.version}", "bPermissions"),
     B_PERMISSIONS2("bPermissions2", "${bpermissions.version}", "bPermissions"),
     DROX_PERMS("DroxPerms", "${droxperms.version}", "DroxPerms"),
     GROUP_MANAGER("GroupManager", "${groupmanager.version}", "GroupManager"),
-    K_PERMS("KPerms", "${kperms.version}", "KPerms"),
-    LUCK_PERMS("LuckPerms", "${luckperms.version}", "LuckPerms"),
+    I_CHAT("iChat", "${ichat.version}", "iChat"),
+    M_CHAT("mChat", "${mchat.version}", "mChat"),
+    M_CHAT_SUITE("mChatSuite", "${mchatsuite.version}", "mChatSuite"),
     OVER_PERMISSIONS("OverPermissions", "${overpermissions.version}", "OverPermissions"),
     PERMISSIONS3("Permissions3", "${permissions.version}", "Permissions"),
-    PERMISSIONS_BUKKIT("PermissionsBukkit", "${permissionsbukkit.version}", "PermissionsBukkit"),
     PERMISSIONS_EX("PermissionsEx", "${permissionsex.version}", "PermissionsEx"),
     PRIVILEGES("Privileges", "${privileges.version}", "Privileges"),
     RSC_PERMISSIONS("rscPermissions", "${rscpermissions.version}", "rscPermissions"),
-    SIMPLY_PERMS("SimplyPerms", "${simplyperms.version}", "SimplyPerms"),
-    STARBURST("Starburst", "${starburst.version}", "Starburst"),
-    TOTAL_PERMISSIONS("TotalPermissions", "${totalpermissions.version}", "TotalPermissions"),
-    V_PERMS("vPerms", "${permissions.version}", "vPerms"),
-    X_PERMS("Xperms", "${xperms.version}", "Xperms");
+    TOTAL_PERMISSIONS("TotalPermissions", "${totalpermissions.version}", "TotalPermissions");
 
     @NotNull
     private final String name;
@@ -33,39 +29,39 @@ public enum PermissionPlugin {
     @NotNull
     private final String pluginName;
 
-    PermissionPlugin(@NotNull String name, @NotNull String version, @NotNull String pluginName) {
+    ChatPlugin(@NotNull String name, @NotNull String version, @NotNull String pluginName) {
         this.name = name;
         this.version = version;
         this.pluginName = pluginName;
     }
 
     /**
-     * Gets Vault's internal name of the permission plugin.
+     * Gets Vault's internal name of the chat plugin.
      *
-     * @return the internal name of the permission plugin.
+     * @return the internal name of the chat plugin.
      */
     @NotNull
     public String getName() { return name; }
 
     /**
-     * Gets the permission plugin's version.
+     * Gets the chat plugin's version.
      *
-     * @return the version of the permission plugin.
+     * @return the version of the chat plugin.
      */
     @NotNull
     public String getVersion() { return version; }
 
     /**
-     * Gets the permission plugin's name as is provided to/by Bukkit.
+     * Gets the chat plugin's name as is provided to/by Bukkit.
      *
-     * @return the permission plugin's name
+     * @return the chat plugin's name
      */
     @NotNull
     public String getPluginName() { return pluginName; }
 
     @Override
     public String toString() {
-        return "PermissionPlugin{" +
+        return "ChatPlugin{" +
                 "name='" + name + '\'' +
                 ", version='" + version + '\'' +
                 ", pluginName='" + pluginName + '\'' +
